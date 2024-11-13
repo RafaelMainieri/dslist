7b2d636fb4ae84d957dfaa5ee6fb6ee7ada51ca3
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-11-13 20:55:35 -0300

    Criação do Service, Repository, Controller e DTO de GameList

---
+++ src/main/java/com/devsuperior/dslist/repositores/GameListRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslist.repositores;
+
+import com.devsuperior.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositores.GameListRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    private GameListRepository gameListRepository;
+
+    public GameListService(GameListRepository gameListRepository) {
+        this.gameListRepository = gameListRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
