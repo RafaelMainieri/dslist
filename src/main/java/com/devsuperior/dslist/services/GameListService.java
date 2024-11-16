5c190cdd663bf37d478d2ad1995650cc76f75cb5
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-11-16 12:04:03 -0300

    Implementação da função move (para mover os jogos na lista)

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositores.GameListRepository;
+import com.devsuperior.dslist.repositores.GameRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +15,31 @@ public class GameListService {
 
     private GameListRepository gameListRepository;
 
-    public GameListService(GameListRepository gameListRepository) {
+    private GameRepository gameRepository;
+
+    public GameListService(GameListRepository gameListRepository, GameRepository gameRepository) {
         this.gameListRepository = gameListRepository;
+        this.gameRepository = gameRepository;
     }
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i < max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
